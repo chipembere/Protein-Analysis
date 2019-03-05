5e6f54d3f221c461ce1fb4c806f0dad279774a27
chipembere <34651984+chipembere@users.noreply.github.com>
2019-03-05 13:37:16 +0000

    Add files via upload

--- arguments.py
+++ arguments.py
@@ -5,11 +5,13 @@ from itertools import combinations
 from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from Bio import AlignIO
+import texttable as tt
 
-
-limit_results = 5
+#limit the number of proteins gathered
+limit_results = 5 #could be argument 3
 tei = []
 count = 0
+tab = tt.Texttable()
 
 ##path to Uniprot XML file##
 uniprot_xml = sys.argv[1]
@@ -17,7 +19,7 @@ uniprot_xml = sys.argv[1]
 f = open(uniprot_xml)
 f = SeqIO.parse(f, "uniprot-xml")
 
-#find proteins invloved in catabolic activity from Uniprot XML
+#find proteins invloved in some similar activity/function from Uniprot XML
 for entry in f:
     #print(entry.annotations['keywords'])
     if ("comment_function" in entry.annotations.keys()):
@@ -32,17 +34,31 @@ for entry in f:
             if (count == limit_results):
                 break
 
-f = open(sys.argv[2] + ".fasta", 'w')
+path = sys.argv[2] + ".fasta"
+f = open(path, 'w')
 SeqIO.write(tei, f, "fasta")
+
+#Put the locations/keywords of the proteins in a table
+for entry in tei:
+    if ("comment_subcellularlocation_location", "keywords" in entry.annotations.keys()):
+        headings = ["Entry ID", "Keywords"]
+        tab.header(headings)
+        tab.add_row([entry.id, entry.annotations["keywords"]])
+
+s = tab.draw()
+with open("Table.txt", 'w') as f:
+    f.write(s)
+    f.close()
 
 #scoring and alignment function
 
 #file = sys.argv[2] + ".fasta"
-file = "catabolism.fasta"
-print(sys.argv)
+#file = "catabolism.fasta"
+#print(sys.argv)
 def seq_comp(file):
     seq_r = SeqIO.parse(file, "fasta")
-    print([i for i in seq_r])
+    
+    # print([i for i in seq_r])
     for record1, record2 in combinations(seq_r, 2):
         seq1 = record1.seq
         seq2 = record2.seq
@@ -52,4 +68,6 @@ def seq_comp(file):
             q.write(format_alignment(*alignments[0:]))
 
 #print(seq_comp(file))
-seq_comp(file)
+f = open(path, 'r')
+# print(type(f))
+seq_comp(path)
