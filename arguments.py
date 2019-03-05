e8b19d02b8115d48af9147130e59a41d8fdc1ed2
chipembere <34651984+chipembere@users.noreply.github.com>
2019-03-05 15:44:23 +0000

    Add files via upload
    added keyword/cellular location word frequency counter

--- arguments.py
+++ arguments.py
@@ -6,12 +6,14 @@ from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from Bio import AlignIO
 import texttable as tt
+from collections import Counter
 
 #limit the number of proteins gathered
 limit_results = 5 #could be argument 3
 tei = []
 count = 0
 tab = tt.Texttable()
+most_commn = []
 
 ##path to Uniprot XML file##
 uniprot_xml = sys.argv[1]
@@ -38,6 +40,13 @@ path = sys.argv[2] + ".fasta"
 f = open(path, 'w')
 SeqIO.write(tei, f, "fasta")
 
+# this functions is used to count the most common keyword or location, 
+# this can be used to find out if a particular function only occurs in a certain location
+def Most_Common(lst): 
+    data = Counter(lst)
+    return data.most_common(1)[0][0]
+
+
 #Put the locations/keywords of the proteins in a table
 for entry in tei:
     if ("comment_subcellularlocation_location", "keywords" in entry.annotations.keys()):
@@ -45,9 +54,18 @@ for entry in tei:
         tab.header(headings)
         tab.add_row([entry.id, entry.annotations["keywords"]])
 
+        #computing the most frequent keyword/cellular location
+        most_commn.append([entry.annotations["keywords"]])
+        chero = [i for l in most_commn for i in l]  
+        cg = [i for l in chero for i in l]
+        mst_cmn_w = Most_Common(cg)
+
+qs = "\nThe most frequent keywords phrase or cellular location is: \n" + mst_cmn_w
+#print("\nThe most frequent keywords/location are; \n" + mst_cmn_w)        
 s = tab.draw()
-with open("Table.txt", 'w') as f:
+with open("Table_n_MstCmnKywdOrLctn.txt", 'w') as f:
     f.write(s)
+    f.write(qs)
     f.close()
 
 #scoring and alignment function
@@ -64,7 +82,7 @@ def seq_comp(file):
         seq2 = record2.seq
         q = open(sys.argv[2] + ".txt", 'w')
         for alignments in pairwise2.align.globalxx(seq1, seq2):
-            print("Done")
+            #print("Done")
             q.write(format_alignment(*alignments[0:]))
 
 #print(seq_comp(file))
