4efb5936b9a2664d488b9d01ba061cb5cf627075
Brian Musonza <brianmusonza@neweduroam-10-61-5-164.ncl.ac.uk>
2019-03-07 14:07:08 +0000

    Stage 8

--- Proto.py
+++ Proto.py
@@ -1,20 +1,20 @@
-'''This is a command line tool for protein analysis using UniProt XML, Clustalw2
-(you will need to download both the Uniprot xml and Clustalw2), and
-a few other libraries. The idea is to select proteins related by function, 
+'''This is a command line tool for protein analysis using uniprot_sprot.xml.gz, 
+clustalw2 (which you will need to download including biopython and 
+texttable modules). The idea is to select proteins related by function 
 for example, if we wanted to study proteins that are associated with catabolism 
-we enter the name of this python file in the command line along with path 
-to the xml file and the word 'catabolism'. The programme is set to select 25 
-proteins but you can open the Proto.py file and change the limit_results value. 
-The programme will ask for the path to clustalw2, copy the path without
-spaces and enter. The program will write a number of files, including 
-an aln and a text files where you can view alignments. There are two alignment
- files to compare presentation and notation. The outputs also include a fasta 
- file of the selected proteins and a .txt file with a table that shows the 
- entry ID and associated cellular location for each protein
-and at the end of that file it says which is the most common cellular location
-(this is useful because it may highlight if a cellular process only occurs in a 
-particular cellular location). A phylogenetic tree is also printed out to the 
-command line for assesing the distance between the proteins.'''
+we enter the name of this python file in the command line along with path to 
+the uniprot_xml.gz file and the word 'catabolism'. The programme is set to 
+select 25 proteins but you can open the Proto.py file and change the limit_results 
+value. The programme will ask for the path to clustalw2, copy the path without 
+spaces and enter. The program will write a number of files, including an aln 
+and a text files where you can view alignments. There are two alignment files 
+to compare presentation and notation. The outputs also include a fasta file of the 
+selected proteins and a .txt file with a table that shows the entry ID and 
+associated cellular location for each protein and at the end of that file it 
+tells you the most common cellular location (this is useful because it may highlight 
+if a cellular process only occurs in a particular cellular location). A phylogenetic 
+tree is also printed out to the command line for assesing the distance between the 
+proteins.'''
 
 import sys
 from Bio import SeqIO
